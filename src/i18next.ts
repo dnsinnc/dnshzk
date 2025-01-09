03a815f4cfaeffe50d48d1af345a5097407bc131
denis huziuk <denishuziuk@gmail.com>
2025-01-09 19:35:23 +0100

    bag fix 2

--- src/i18next.ts
+++ src/i18next.ts
@@ -14,7 +14,7 @@ i18n
             escapeValue: false,
         },
         backend: {
-            loadPath: '/locales/{{lng}}/translation.json',
+            loadPath: 'dnshzk/locales/{{lng}}/translation.json',
         },
         react: {
             useSuspense: true,
